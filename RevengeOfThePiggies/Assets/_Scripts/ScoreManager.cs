583abab84e3945d7e3c4aeaba3c823de321fd5f9
auideaslab <krzysztofvr@gmail.com>
2019-09-18 13:01:35 -0400

    Singleton
    Singleton

---
+++ RevengeOfThePiggies/Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager
+{
+    public int score = 0;
+
+    public static ScoreManager instance = new ScoreManager();
+
+    // making the constructor private prevents others 
+    // from creating an object of this class
+
+    private ScoreManager()
+    {
+
+    }
+}
